86f4a1c66ce2bbb419e39b6a69283afd2e0c96f9
Neposeda644 <denis_bulyga@mail.ru>
2021-01-12 10:35:30 +0300

    done result

--- src/CreditPaymentService.java
+++ src/CreditPaymentService.java
@@ -1,8 +1,8 @@
 public class CreditPaymentService {
-    public double calculate (double percent, int time, double sum){
-        double percentmounth = percent/100/12;
-        double pow = (long) Math.pow(1+percent,time);
-        double mounthpay = sum * (percentmounth*pow)/(pow-1);
+    public double calculate(double percent, int time, double sum) {
+        double percentmounth = percent / 100 / 12;
+        double pow = Math.pow(1 + percentmounth, time);
+        double mounthpay = sum * (percentmounth * pow) / (pow - 1);
         return mounthpay;
     }
 
--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,19 @@
 public class Main {
     public static void main(String[] args) {
         CreditPaymentService service = new CreditPaymentService();
+
         double mounthpay = service.calculate(9.99,12,1000000);
+        System.out.printf("%.0f\n", mounthpay);
 
+        CreditPaymentService service1 = new CreditPaymentService();
 
-        System.out.println(mounthpay);
+        double mounthpay1 = service.calculate(9.99,24,1000000);
+        System.out.printf("%.0f\n", mounthpay1);
+
+        CreditPaymentService service2 = new CreditPaymentService();
+
+        double mounthpay2 = service.calculate(9.99,36,1000000);
+        System.out.printf("%.0f\n", mounthpay2);
 
     }
 }
